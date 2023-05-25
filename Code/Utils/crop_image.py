645dfd29826aebe4f61c5434b20d3e8f84f9c115
Chenye Yang <cyyyang@ucdavis.edu>
2023-05-24 18:24:31 -0700

    crop image

---
+++ Code/Utils/crop_image.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+# List of image paths
+image_paths = ["Resource/image.png", "Result/hamming-bsc-output.png", "Result/hamming-bsc-output-corrected.png"]
+
+# The area to be cropped out of the images: (left, upper, right, lower)
+crop_area = (550, 550, 750, 750)
+
+# Loop through all images
+for image_path in image_paths:
+    # Open an image file
+    with Image.open(image_path) as img:
+        # Crop the image
+        cropped_img = img.crop(crop_area)
+        # Save the cropped image
+        cropped_img.save(image_path.split("/")[0] + "/cropped-" + image_path.split("/")[-1])
