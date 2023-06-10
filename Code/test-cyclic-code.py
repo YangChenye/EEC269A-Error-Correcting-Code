57afd1f22d55b5bbae42913bf838e2b7d893a606
Chenye Yang <cyyyang@ucdavis.edu>
2023-06-10 11:19:00 -0700

    Create test-cyclic-code.py

---
+++ Code/test-cyclic-code.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2023 Chenye Yang
+
+from Channel import channel
+from Utils import polyTools
+
+import numpy as np
+
+
+tx_msg = np.array([0, 1, 0, 0, 1, 0, 0, 0, 0, 1], dtype=np.uint8) # 10 bits
+print("Original bits:", tx_msg)
+
+# Channel
+chl = channel.Channel()
+
+# Work with (15, 11) cyclic code
+n = 15
+k = 11
+
+# Create the generator matrix
+genMatrix_decimal = polyTools.findMatrix(n, k)
+G = polyTools.genMatrixDecmial2Ndarray(genMatrix_decimal, n)
+
+
+cyclic_code = channel.Cyclic_Code(G)
+
+# Encoding
+tx_codewords = cyclic_code.encoder_systematic(tx_msg)
+print("Encoded bits      :", tx_codewords)
+
+# Passing through the channel
+rx_codewords = chl.binary_symmetric_channel(tx_codewords, 0.1)
+print("Bits after channel:", rx_codewords)
+
+# Correction with syndrome look-up table
+estimated_tx_codewords = cyclic_code.corrector_syndrome(rx_codewords)
+print("Estimated TX bits :", estimated_tx_codewords)
+
+# Decoding
+padding_length = k - (len(tx_msg) % k)
+rx_msg = cyclic_code.decoder_systematic(estimated_tx_codewords, padding_length)
+print("Decoded bits :", rx_msg)
